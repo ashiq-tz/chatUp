65bdbb2f29179541cfd6f763aceb71c3264484e9
ashiq <ashiq@ashiqs-MacBook-Air.local>
2025-11-06 20:02:12 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth",authRoutes)
 app.use("api/messages", messageRoutes)
+
+//make ready for deployment (sevalla)
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log("Server running on port: " + PORT)
