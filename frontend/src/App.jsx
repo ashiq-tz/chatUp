4c0b2b226f2fcd278092f88c4c2545241fa99766
ashiq-tz <ashiqthazhath@gmail.com>
2025-11-08 23:22:28 +0530

    frontend code initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ChatPage from './pages/ChatPage'
+import { Route, Routes } from 'react-router'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-gradient-to-br from-[#dbeafe] via-[#bfdbfe] to-[#93c5fd] relative flex items-center justify-center p-4 overflow-hidden'>
+
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_top_left,rgba(255,255,255,0.3)_0%,transparent_70%),radial-gradient(circle_at_bottom_right,rgba(59,130,246,0.2)_0%,transparent_70%)]" />
+      
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+      
+    </div>
+
   )
 }
 
