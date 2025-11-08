8551c0fd284707755a3ee028bae74c48e806976d
ashiq-tz <ashiqthazhath@gmail.com>
2025-11-08 19:22:13 +0530

    message routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -43,6 +43,19 @@ export const sendMessage = async (req,res) => {
         const {id: receiverId} = req.params
         const senderId = req.user._id
 
+        //code rabbit suggestion //
+        if(!text && !image){
+            return res.status(400).json({message: "Text or image is required"})
+        }
+        if(senderId.equals(receiverId)) {
+            return res.status(400).json({message: "Cannot send msg to yourself"})
+        }
+        const receiverExists = await User.exists({ _id: receiverId })
+        if(!receiverExists){
+            return res.status(400).json({message: "Receiver not found"})
+        }
+        //
+
         let imageUrl;
 
         if(image){
