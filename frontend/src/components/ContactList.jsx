4fa54701e13fb54bbd191fdc7b8593cc78b355f8
ashiq-tz <ashiqthazhath@gmail.com>
2025-11-12 20:29:38 +0530

    sidebar setup completed

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore.js';
+import { useAuthStore } from '../store/useAuthStore.js';
+import UsersLoadingSkeleton from './UsersLoadingSkeleton.jsx';
+
 
 const ContactList = () => {
+
+  const { getAllContacts, allContacts, setSelectedUser, isUsersLoading } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+  if (isUsersLoading) return <UsersLoadingSkeleton />
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {allContacts.map((contact) => (
+        <div
+          key={contact._id}
+          className="bg-blue-500/10 p-4 rounded-lg cursor-pointer hover:bg-blue-500/20 transition-colors"
+          onClick={() => setSelectedUser(contact)}
+        >
+          <div className="flex items-center gap-3">
+            <div className={`avatar online`}>
+              <div className="size-12 rounded-full">
+                <img src={contact.profilePic || "/avatar.png"} />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium">{contact.fullName}</h4>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
